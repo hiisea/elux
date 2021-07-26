78945cd62f0b0187ca0def0d7095a67accf9d6e6
hiisea <hiisea@qq.com>
2021-07-26 19:11:27 +0800

    fix: 小程序regenerator-runtime 只支持0.11.1

--- packages/react-redux-taro/rollup.config.js
+++ packages/react-redux-taro/rollup.config.js
@@ -1,3 +1,3 @@
 import build from '../../rollup.build';
-const config = build(__dirname);
+const config = build(__dirname, undefined, {'regenerator-runtime': require.resolve('regenerator-runtime')});
 export default config;
