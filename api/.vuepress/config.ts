6f6d5ad157bce0390c1c0e1e0bf18448270d40ae
hiisea <hiisea@qq.com>
2022-05-12 15:11:55 +0800

    docs: 将api文档单独构建

---
+++ api/.vuepress/config.ts
@@ -0,0 +1,75 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import {defineUserConfig} from 'vuepress';
+import {defaultTheme} from '@vuepress/theme-default';
+
+export default defineUserConfig({
+  shouldPrefetch: false,
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'Hello-Elux',
+      description: 'Elux不只是一个JS框架，更是一种基于“微模块”和“模型驱动”的跨平台、跨框架同构方案',
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'Hello-Elux',
+      description: 'Elux不只是一个JS框架，更是一种基于“微模块”和“模型驱动”的跨平台、跨框架同构方案',
+    },
+  },
+  theme: defaultTheme({
+    darkMode: false,
+    editLink: false,
+    logo: '/images/logo-2.svg',
+    repo: 'hiisea/elux',
+    docsDir: 'api',
+    locales: {
+      '/': {
+        selectLanguageName: '简体中文',
+        navbar: [
+          {
+            text: '设计思想',
+            link: '/designed/micro-module.html',
+          },
+          {
+            text: '开发指南',
+            link: '/guide/summary.html',
+          },
+          {
+            text: 'API文档',
+            link: '/api/',
+          },
+          {
+            text: 'CLI工具',
+            link: '/cli/',
+          },
+        ],
+        sidebar: [
+          {
+            text: 'API手册',
+            children: [
+              {
+                text: '@elux/react-web',
+                link: '/api/react-web.md',
+              },
+              {
+                text: '@elux/react-taro',
+                link: '/api/react-taro.md',
+              },
+              {
+                text: '@elux/vue-web',
+                link: '/api/vue-web.md',
+              },
+              {
+                text: '@elux/vue-taro',
+                link: '/api/vue-taro.md',
+              },
+            ],
+          },
+        ],
+      },
+      '/en/': {
+        selectLanguageName: 'English',
+      },
+    },
+  }),
+});
--- patch-api.js
+++ patch-api.js
@@ -11,23 +11,15 @@ const indexPage = `# API手册
 - [@elux/vue-taro](./vue-taro.md)
 `;
 replace.sync({
-  files: './docs/api/index.md',
+  files: './api/api/index.md',
   from: /[\d\D]*/,
   to: indexPage,
   countMatches: true,
 });
 
 replace.sync({
-  files: ['./docs/api/react-web.md', './docs/api/react-taro.md', './docs/api/vue-web.md', './docs/api/vue-taro.md'],
+  files: ['./api/api/react-web.md', './api/api/react-taro.md', './api/api/vue-web.md', './api/api/vue-taro.md'],
   from: [/## Interfaces[\d\D]*?## Variables/, /\n## Type Aliases[\d\D]*?$/],
   to: ['## Variables', ''],
   countMatches: true,
 });
-// const options = {
-//   files: './docs/api/index.md',
-//   from: /"@babel\/runtime":\s?"([^"]+)"/g,
-//   to: `"@babel/runtime": "~"`,
-//   countMatches: true,
-// };
-
-// replace.sync(options);
