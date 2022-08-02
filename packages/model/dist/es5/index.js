f7a456a337c55690f6b9d231a7ec7f69947bedaf
hiisea <hiisea@qq.com>
2022-08-02 11:55:22 +0800

    refactor: 整理@elux/model对外api

--- packages/model/dist/es5/index.js
+++ packages/model/dist/es5/index.js
@@ -1,1 +1,25 @@
+import { setCoreConfig, getModuleApiMap } from '@elux/core';
 export { BaseModel, EmptyModel, ErrorCodes, deepMerge, effect, effectLogger, env, errorAction, exportComponent, exportModule, exportView, getApi, getTplInSSR, injectModule, isMutable, isServer, locationToNativeLocation, locationToUrl, modelHotReplacement, moduleExists, nativeLocationToLocation, nativeUrlToUrl, reducer, setLoading, urlToLocation, urlToNativeUrl } from '@elux/core';
+
+var appConfig = Symbol();
+function setConfig(conf) {
+  setCoreConfig(conf);
+
+  if (conf.DisableNativeRouter) {
+    setCoreConfig({
+      NotifyNativeRouter: {
+        window: false,
+        page: false
+      }
+    });
+  }
+
+  return appConfig;
+}
+function patchActions(typeName, json) {
+  if (json) {
+    getModuleApiMap(JSON.parse(json));
+  }
+}
+
+export { patchActions, setConfig };
